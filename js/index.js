7e39ab4ee8eaa9441bb81481925143fc347d8313
miyeong <kmy9810@naver.com>
2023-05-26 14:59:40 +0900

    최근 본 레시피 추가, 페이지네이션 수정

--- js/index.js
+++ js/index.js
@@ -1,87 +1,68 @@
-let recipe_data = []
-let page = 8
+let category_id = "1";
+let recipe_total = 0
+// let last_page = parseInt(recipe_total / 8) + 2
 
 
-async function RecipeList(event) {
+function RecipeList(event) {
     // const accessToken = localStorage.getItem('access')
-    localStorage.setItem("page", 8)
-    page = 8
-    var category_id = "1";
     if (event && event.target) {
         category_id = event.target.id.slice(-1);
     }
-    const response = await fetch(`http://127.0.0.1:8000/recipe-list/${category_id}/`, {
-        headers: {
-            'content-type': 'application/json',
-            // 'Authorization': `Bearer ${accessToken}`
-        },
-        method: 'GET',
-    })
+    localStorage.setItem("page", 0)
 
-    const response_json = await response.json()
-    $('#recipe_list').empty()
-    $("#m_btn").hide();
-
-    for (step = 0; step < 8; step++) {
-        const name = response_json[step]['name']
-        const main_img = response_json[step]['main_img']
-        const kcal = response_json[step]['kcal']
-        const recipe_id = response_json[step]['id']
-
-        let temp_html1 = `<a href="/recipe_detail.html?recipe_id=${recipe_id}">
-                            <section class="cp-card content" style="height:300px; padding:10px;">
-                                <div class="thumb" style="background-image: url(${main_img}); border-radius:20%">
-                                </div>
-                                <div class="body">
-                                    <h4 style="font-size:15px;">${name}</h4>
-                                    <div class="metadata" style="margin-top:2px; float:right;">
-                                        <div class="review-rating">
-                                            <span class="cp-stars"style="font-size:15px;">
-                                                ${kcal}kcal
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </section>
-                        </a>`;
-        $('#recipe_list').append(temp_html1);
-    }
-    recipe_data = []
-    recipe_data.push(response_json)
-    last_page = parseInt(recipe_data[recipe_data.length - 1].length / 8 - 2)
-    $('#page_num').empty()
-    let page_num_html = `<h5>1 페이지 / ${last_page + 2} 페이지</h5>`
-    $('#page_num').append(page_num_html);
-    $("#p_btn").show();
-
+    recipe_total = 0
+    // 페이징을 위한 숫자 불러오기
+    PageList()
 }
 
 RecipeList()
 
 
+async function PageList() {
+    // 첫페이지 로드 될 때가지 기다리기 -> recipe_total 얻기 위해!
+    await P_Page()
 
-function P_Page() {
-    console.log(recipe_data)
-    local_page = localStorage.getItem('page')
-    last_page = parseInt(recipe_data[recipe_data.length - 1].length / 8 - 2)
-    if (parseInt(local_page) / 8 > last_page) {
-        $("#p_btn").hide();
-    }
-    $("#m_btn").show();
-    page += 8
-    $('#recipe_list').empty()
-    $('#page_num').empty()
-    let page_num_html = `<h5>${parseInt(local_page) / 8 + 1} 페이지 / ${last_page + 2} 페이지</h5>`
-    $('#page_num').append(page_num_html);
-    for (step = page; step < page + 8; step++) {
-        // console.log(recipe_data[0][step])
-        const data_page = recipe_data[0][step]
-        const name = data_page['name']
-        const main_img = data_page['main_img']
-        const kcal = data_page['kcal']
-        const recipe_id = data_page['id']
+    let total_page = parseInt(recipe_total / 8) + 1
+    // console.log(total_page)
 
-        let temp_html1 = `<a href="/recipe_detail.html?recipe_id=${recipe_id}">
+    // 현재 보고 있는 페이지, 이동할 수 있는 번호, 생략해야하는 번호, 보여줄 한계치(10개? 5개?)
+    // $('#page_num_list').empty()
+    // for (step = 1; step < `${total_page + 1}`; step++) {
+    //     let page_list_html = `<a href='' id='${step}page' style="margin-left:20px; font-size:25px;">${step}</a>`
+    //     console.log(typeof (`${step}page`))
+    //     $('#page_num_list').append(page_list_html)
+    // }
+}
+
+
+async function P_Page() {
+    let p_offset = localStorage.getItem('page') * 1
+    if (p_offset > recipe_total) {
+        alert("마지막 페이지 입니다")
+    } else {
+        const response = await fetch(`http://127.0.0.1:8000/recipe-list/${category_id}/${p_offset}`, {
+            headers: {
+                'content-type': 'application/json',
+                // 'Authorization': `Bearer ${accessToken}`
+            },
+            method: 'GET',
+        })
+        const response_json = await response.json()
+        //레시피 총개수 한번만 저장
+        if (recipe_total == 0) {
+            recipe_total = response_json['total']
+        }
+
+
+        $('#recipe_list').empty()
+
+        response_json['data'].forEach((a) => {
+            const name = a['name']
+            const main_img = a['main_img']
+            const kcal = a['kcal']
+            const recipe_id = a['id']
+
+            let temp_html1 = `<a href="/recipe_detail.html?recipe_id=${recipe_id}">
                                 <section class="cp-card content" style="height:300px; padding:10px;">
                                     <div class="thumb" style="background-image: url(${main_img}); border-radius:20%">
                                     </div>
@@ -97,25 +78,50 @@ function P_Page() {
                                     </div>
                                 </section>
                             </a>`;
-        $('#recipe_list').append(temp_html1);
+            $('#recipe_list').append(temp_html1);
+        })
+        localStorage.setItem('page', p_offset + 8)
     }
-    localStorage.setItem('page', page)
-
+    // 현재 페이지 위치를 보여주는 곳
+    $('#page_num').empty()
+    let page_num_html = `<h5>${p_offset / 8 + 1} 페이지 / ${parseInt(recipe_total / 8) + 1} 페이지</h5>`
+    $('#page_num').append(page_num_html);
+    //해당 번호 a태그 css 변경
+    // let target_num = document.getElementById(`${p_offset / 8 + 1}page`)
+    // console.log(target_num)
+    // if (target_num != null) {
+    //     target_num.style.color = "blue";
+    //     target_num.style.fontSize = "30px";
+    // }
 
 }
 
-function M_Page() {
-    local_page = localStorage.getItem('page')
-    page -= 8
+
+
+async function M_Page() {
+    //다음 버튼 눌렀다 뒤로 오면 -16~ -> -8 하면 그자리
+    let m_offset = localStorage.getItem('page') * 1 - 16
+    if (localStorage.getItem('page') * 1 - 8 <= 0) {
+        alert('첫 페이지 입니다!')
+    } else {
+        localStorage.setItem('page', m_offset + 8)
+    }
+
+    const response = await fetch(`http://127.0.0.1:8000/recipe-list/${category_id}/${m_offset}`, {
+        headers: {
+            'content-type': 'application/json',
+            // 'Authorization': `Bearer ${accessToken}`
+        },
+        method: 'GET',
+    })
+    const response_json = await response.json()
     $('#recipe_list').empty()
 
-    for (step = page - 8; step < page; step++) {
-        // console.log(recipe_data[0][step])
-        const data_page = recipe_data[0][step]
-        const name = data_page['name']
-        const main_img = data_page['main_img']
-        const kcal = data_page['kcal']
-        const recipe_id = data_page['id']
+    response_json['data'].forEach((a) => {
+        const name = a['name']
+        const main_img = a['main_img']
+        const kcal = a['kcal']
+        const recipe_id = a['id']
 
         let temp_html1 = `<a href="/recipe_detail.html?recipe_id=${recipe_id}">
                             <section class="cp-card content" style="height:300px; padding:10px;">
@@ -134,20 +140,58 @@ function M_Page() {
                             </section>
                         </a>`;
         $('#recipe_list').append(temp_html1);
-        // for (const [k, v] of Object.entries(recipe_data[0][step])) {
-        //     console.log(k, v)
-        // }
+    })
 
-    }
-    localStorage.setItem('page', page)
-    if (localStorage.getItem('page') <= 8) {
-        $("#m_btn").hide();
-    } else {
-        $("#p_btn").show();
-    }
-    local_page = localStorage.getItem('page')
     $('#page_num').empty()
-    let page_num_html = `<h5>${parseInt(local_page) / 8} 페이지 / ${last_page + 2} 페이지</h5>`
+    let page_num_html = `<h5>${m_offset / 8 + 1} 페이지 / ${parseInt(recipe_total / 8) + 1} 페이지</h5>`
     $('#page_num').append(page_num_html);
+
 }
 
+// 가장 최근에 본 레시피
+async function LastRecipe() {
+    if (localStorage.getItem('last_watch_recipe')) {
+        let last_watch_recipe = localStorage.getItem('last_watch_recipe')
+        const response = await fetch(`http://127.0.0.1:8000/recipe/${last_watch_recipe}/`, {
+
+            headers: {
+                "Content-Type": "application/json",
+                // Authorization: "Bearer " + localStorage.getItem("access"),
+            },
+            method: "GET",
+        });
+        const response_json = await response.json()
+        console.log(response_json)
+        const main_img = response_json['main_img']
+        const name = response_json['name']
+        const kcal = response_json['kcal']
+        const tip = response_json['tip']
+        const ingredients = response_json['ingredients']
+
+        let temp_html1 = `<a href="/recipe_detail.html?recipe_id=${last_watch_recipe}">
+                            <section class="cp-card content" style="height: auto; overflow:hidden; padding:10px;">
+                                <div class="thumb" style="background-image: url(${main_img}); border-radius:20%; height:300px; width:500px;
+                                                        margin:30px auto 30px 20px; display: inline-block;"></div>
+                                <div class="body" style="width:400px; float:right; margin-right:50px; text-align:center;">
+                                    <h4 >${name}</h4>
+                                    <span class="cp-stars" >
+                                            ${kcal}kcal 
+                                    </span>
+                                    <div class="metadata" style="float:right; margin-top:20px">
+                                        <p>TIP : ${tip}</p>
+                                    </div>
+                                    <div class="metadata" style="float:right; margin-top:20px">
+                                        <p>준비물 : ${ingredients}</p>
+                                    </div>
+                                </div>
+                            </section>
+                        </a>`;
+        $('#last_wathc_list').append(temp_html1);
+
+
+
+
+    }
+}
+
+LastRecipe()
